89801f17b32d10b3484742831241572c8447f92c
Alejandro <174962913+alegarlinx@users.noreply.github.com>
2026-09-23 10:30:03 +0200

    Delete pqc_radar directory

--- pqc_radar/__init__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-# Copyright 2026 Alejandro Garcia Linero
-"""PQC-Radar: inventario criptográfico y priorización post-cuántica con políticas europeas."""
-__version__ = "0.4.0"
